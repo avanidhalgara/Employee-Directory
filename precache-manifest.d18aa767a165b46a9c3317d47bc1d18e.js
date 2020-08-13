315c4551ff4319c072b184994877c4ec3aa099f2
unknown <avanidhalgara01@gmail.com>
2020-08-13 00:25:30 -0500

    Updates

---
+++ precache-manifest.d18aa767a165b46a9c3317d47bc1d18e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a426d7e7a5ff76a5e939dd240eb76b",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "5d3be6d24149c432905e",
+    "url": "/User-Directory/static/css/main.eb726011.chunk.css"
+  },
+  {
+    "revision": "bbb0b218ccc461cc0f66",
+    "url": "/User-Directory/static/js/2.6e1adf2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.6e1adf2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3be6d24149c432905e",
+    "url": "/User-Directory/static/js/main.6e67c192.chunk.js"
+  },
+  {
+    "revision": "5a32cb48b2efdc341ea6",
+    "url": "/User-Directory/static/js/runtime-main.094f3a12.js"
+  }
+]);
